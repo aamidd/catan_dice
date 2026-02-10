b1641879cc997de6f8e9b130875560a5ef957b41
amid <sanaiamid@gmail.com>
2026-02-10 19:03:49 +0330

    refactor(messages): add newlines to messages

--- dice.py
+++ dice.py
@@ -15,16 +15,16 @@ def query_number_of_players():
         try:
             n = int(raw_input)
         except ValueError:
-            print("!!! enter a number !!!")
+            print("\n!!! enter a number !!!\n")
             continue
         if 1 <= n <= 6:
             return n
-        print("!!! number of players should be between 1-6 !!!")
+        print("\n!!! number of players should be between 1-6 !!!\n")
 
 n = query_number_of_players() # number of players
 players = []
 
-print("*** enter the names of the players in the same order as you're sitting ***")
+print("\n*** enter the names of the players in the same order as you're sitting ***\n")
 for i in range(n):
     players += [input(f"player no.{i + 1}: ")]
     
@@ -32,14 +32,14 @@ for i in range(n):
 print("\nrandomly choosing the first player...")
 curr_player = random.randrange(n)
 print(f"{players[curr_player]} was chosen")
-print("\n*** hit enter to roll the dice or hit q and enter to quit ***")
+print("\n*** hit enter to roll the dice or hit q and enter to quit ***\n")
 
 max_width = max(len(p) for p in players) + 6
 
 while True:
     cmd = input().strip().lower()
     if cmd == "q":
-        print("*** quitting ***")
+        print("\n*** quitting ***\n")
         break
 
     d1, d2, total = roll_two()
